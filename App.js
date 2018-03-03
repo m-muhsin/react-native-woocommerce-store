ec7c1c16495208a6d8f4def2dfcfe0e644ae4a74
Muhammad Muhsin <muhammad.muhseen@gmail.com>
2018-03-03 18:32:12 +0530

    Added drawer navigation

--- App.js
+++ App.js
@@ -1,23 +1,32 @@
 import React from "react";
 import {
-  StyleSheet,
   Text,
-  View,
-  ToolbarAndroid,
-  StatusBar,
-  Button
+  Button,
+  Animated,
+  Easing
 } from "react-native";
-import { StackNavigator } from "react-navigation";
+import { StackNavigator, DrawerNavigator } from "react-navigation";
 
 import HomePage from './components/Home/HomePage';
 import Products from "./components/Products/ProductList";
 import Product from "./components/Products/Product";
+import CartPage from './components/CartPage';
+import DrawerContainer from './components/DrawerContainer';
 
-const App = StackNavigator({
+// https://github.com/react-community/react-navigation/issues/1254
+const noTransitionConfig = () => ({
+  transitionSpec: {
+    duration: 0,
+    timing: Animated.timing,
+    easing: Easing.step0
+  }
+});
+
+const DrawerStack = DrawerNavigator({
   Home: {
     screen: HomePage,
     navigationOptions: {
-      title: "Home"
+      title: "Laccadive IO Store"
     }
   },
   Products: {
@@ -31,7 +40,59 @@ const App = StackNavigator({
     navigationOptions: ({ navigation }) => ({
       title: navigation.state.params.product.name
     }),
+  },
+  CartPage: {
+    screen: CartPage,
+    navigationOptions: {
+      title: "Cart"
+    }
   }
-});
+}, {
+    gesturesEnabled: false,
+    contentComponent: DrawerContainer
+  });
+
+const drawerButton = (navigation) => (
+  <Text
+    style={{ padding: 5, color: 'white' }}
+    onPress={() => {
+      if (navigation.state.index === 0) {
+        navigation.navigate('DrawerOpen')
+      } else {
+        navigation.navigate('DrawerClose')
+      }
+    }
+    }>Menu</Text>
+);
+
+const cartButton = (navigation) => <Text style={{ padding: 5, color: 'white' }} onPress={() => {
+ navigation.navigate('CartPage')
+}
+}>Cart</Text>;
+
+const DrawerNavigation = StackNavigator({
+  DrawerStack: { screen: DrawerStack }
+}, {
+    headerMode: 'float',
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: { backgroundColor: '#4C3E54' },
+      headerTintColor: 'white',
+      gesturesEnabled: false,
+      headerLeft: drawerButton(navigation),
+      headerRight: cartButton(navigation)
+    })
+  })
+
+const App = StackNavigator({
+  Home: {
+    screen: DrawerNavigation
+  }
+}, {
+    // Default config for all screens
+    headerMode: 'none',
+    title: 'Main',
+    initialRouteName: 'Home',
+    transitionConfig: noTransitionConfig
+  });
 
 export default App;
---
+++ components/DrawerContainer.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { NavigationActions } from 'react-navigation'
+
+export default class DrawerContainer extends React.Component {
+
+  logout = () => {
+    // This will reset back to loginStack
+    // https://github.com/react-community/react-navigation/issues/1127
+    const actionToDispatch = NavigationActions.reset({
+      index: 0,
+      key: null,  // black magic
+      actions: [NavigationActions.navigate({ routeName: 'loginStack' })]
+    })
+    this.props.navigation.dispatch(actionToDispatch)
+  }
+
+  render() {
+    const { navigation } = this.props
+    return (
+      <View style={styles.container}>
+        <Text
+          onPress={() => navigation.navigate('Home')}
+          style={styles.uglyDrawerItem}>
+          Home
+        </Text>
+        <Text
+          onPress={() => navigation.navigate('Products')}
+          style={styles.uglyDrawerItem}>
+          Products
+        </Text>
+        <Text
+          onPress={() => navigation.navigate('CartPage')}
+          style={styles.uglyDrawerItem}>
+          Cart
+        </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f6f6f6',
+    paddingTop: 40,
+    paddingHorizontal: 20
+  },
+  uglyDrawerItem: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#E73536',
+    padding: 15,
+    margin: 5,
+    borderRadius: 2,
+    borderColor: '#E73536',
+    borderWidth: 1,
+    textAlign: 'center'
+  }
+})
