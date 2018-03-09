4b4906c6f28fc7f459d0a4cce7e38e7a58ffc819
Muhammad Muhsin <muhammad.muhseen@gmail.com>
2018-03-09 11:37:03 +0530

    Product Increase Decrease quantity

--- components/Products/Product.js
+++ components/Products/Product.js
@@ -8,10 +8,24 @@ class Product extends React.Component {
         this.state = { count: 1 };
     }
 
-    onPress = () => {
+    decreaseQuantity = () => {
+        if(this.state.count <= 1) {
+            return;
+        } else {
+            this.setState({
+                count: this.state.count - 1
+            });
+        }
+    }
+
+    increaseQuantitiy = () => {
         this.setState({
-            count: this.state.count -1 + 2
-        })
+            count: this.state.count - 1 + 2
+        });
+    }
+
+    addToCart() {
+        console.log('added');
     }
 
     render() {
@@ -20,18 +34,23 @@ class Product extends React.Component {
             <ScrollView>
                 <Image style={styles.image} source={{ uri: product.images[0].src }} />
                 <Text style={styles.text}>{product.name}     $ {product.price}</Text>
-                <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={(count) => this.setState({ count })}
-                        value={`${this.state.count}`}
-                        keyboardType="numeric"
-                    />
-                    <TouchableOpacity
-                        style={styles.button}
-                        onPress={this.onPress}
-                    >
-                        <Text> ADD TO CART </Text>
+                <View style={{ display: 'flex', flexDirection: 'row', padding: 10, marginLeft: 20, marginBottom: 20 }}>
+                    <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+                        <TouchableOpacity style={styles.decreaseButton} onPress={this.decreaseQuantity}>
+                            <Text> - </Text>
+                        </TouchableOpacity>
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={(count) => this.setState({ count })}
+                            value={`${this.state.count}`}
+                            keyboardType="numeric"
+                        />
+                        <TouchableOpacity style={styles.inceaseButton} onPress={this.increaseQuantitiy} >
+                            <Text> + </Text>
+                        </TouchableOpacity>
+                    </View>
+                    <TouchableOpacity style={styles.button} onPress={this.addToCart} >
+                        <Text style={{ color: '#fff' }}> ADD TO CART </Text>
                     </TouchableOpacity>
                 </View>
                 <HTMLView style={styles.html} value={product.description} />
@@ -55,10 +74,9 @@ const styles = StyleSheet.create({
         height: 40,
         width: 50,
         borderWidth: 1,
-        borderColor: 'gray',
+        borderColor: 'rgba(27,31,35,0.05)',
         padding: 10,
-        marginLeft: 20,
-        marginBottom: 20
+        backgroundColor: 'rgba(27,31,35,0.05)',
     },
     button: {
         alignItems: 'center',
@@ -66,7 +84,27 @@ const styles = StyleSheet.create({
         padding: 10,
         width: 150,
         height: 40,
-        marginLeft: 20
+        marginLeft: 20,
+        borderBottomLeftRadius: 17,
+        borderBottomRightRadius: 17,
+        borderTopLeftRadius: 17,
+        borderTopRightRadius: 17,
+    },
+    decreaseButton: {
+        height: 40,
+        width: 30,
+        padding: 10,
+        backgroundColor: 'rgba(27,31,35,0.05)',
+        borderBottomLeftRadius: 17,
+        borderTopLeftRadius: 17,
+    },
+    inceaseButton: {
+        height: 40,
+        width: 30,
+        padding: 8,
+        backgroundColor: 'rgba(27,31,35,0.05)',
+        borderBottomRightRadius: 17,
+        borderTopRightRadius: 17,
     },
     description: {
         fontSize: 14,
