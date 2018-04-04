e2aaf857cf3d6ec52bfe18bac3d4822f23a562a5
Muhammad Muhsin <muhammad.muhseen@gmail.com>
2018-04-04 15:28:39 +0530

    Cart Count in Header

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import {
 import { StackNavigator, DrawerNavigator } from "react-navigation";
 import { Ionicons, EvilIcons } from '@expo/vector-icons';
 import { Provider } from 'react-redux';
+import { connect } from 'react-redux';
 
 import HomePage from './src/components/Home/HomePage';
 import Products from "./src/components/Products/ProductList";
@@ -49,18 +50,18 @@ const DrawerNavigation = DrawerNavigator({
   });
 
 
-  const StackNavigation = StackNavigator({
-    DrawerNavigation: { screen: DrawerNavigation }
-  }, {
-      headerMode: 'float',
-      navigationOptions: ({ navigation, screenProps }) => ({
-        headerStyle: { backgroundColor: '#4C3E54' },
-        headerTintColor: 'white',
-        gesturesEnabled: false,
-        headerLeft: drawerButton(navigation),
-        headerRight: cartButton(navigation, screenProps)
-      })
-    });
+const StackNavigation = StackNavigator({
+  DrawerNavigation: { screen: DrawerNavigation }
+}, {
+    headerMode: 'float',
+    navigationOptions: ({ navigation, screenProps }) => ({
+      headerStyle: { backgroundColor: '#4C3E54' },
+      headerTintColor: 'white',
+      gesturesEnabled: false,
+      headerLeft: drawerButton(navigation),
+      headerRight: cartButton(navigation, screenProps)
+    })
+  });
 
 const drawerButton = (navigation) => (
   <Text
@@ -75,25 +76,41 @@ const drawerButton = (navigation) => (
     }><Ionicons name="ios-menu" size={30} /></Text>
 );
 
-const cartButton = (navigation, screenProps) => <Text style={{ padding: 15, color: 'white' }} onPress={() => {
-  navigation.navigate('CartPage')
+const cartButton = (navigation, screenProps) => (
+  <Text style={{ padding: 15, color: 'white' }}
+    onPress={() => { navigation.navigate('CartPage') }}
+  >
+    <EvilIcons name="cart" size={30} />
+    {screenProps.cartCount}
+  </Text>
+);
+
+class CA extends React.Component {
+  render() {
+    const cart = {
+      cartCount: this.props.cart.length
+    }
+    return (
+      <StackNavigation screenProps={cart} />
+    );
+  }
 }
-}><EvilIcons name="cart" size={30} />{screenProps.cartCount}</Text>;
+
+function mapStateToProps(state) {
+  return {
+    cart: state.cart
+  };
+}
+
+const ConnectedApp = connect(mapStateToProps, null)(CA);
 
 const store = configureStore();
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cartCount: 0
-    }
-  }
-
   render() {
     return (
       <Provider store={store}>
-        <StackNavigation screenProps={this.state} />
+        <ConnectedApp />
       </Provider>
     )
   }
